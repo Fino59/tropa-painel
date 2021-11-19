aa33a71b6c2bd9d38b10c02f381222a721358f41
Fino59 <slipdan@hotmail.com>
2021-11-19 17:47:04 -0300

    Update Pages Contacts

--- src/components/Pages/Contacts/Create.tsx
+++ src/components/Pages/Contacts/Create.tsx
@@ -29,26 +29,77 @@ const Create: React.FC = (props) => {
             </div>
             <hr></hr>
 
-            <div className="list-contact">
-                <h3 className="title">Escreva sua mensagem</h3>
-                <form>
-                    <input 
-                        type="text"                        
-                        placeholder="Nome" 
-                    />
-                    <input 
-                        type="text"
-                        placeholder="Email" 
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Mensagem"
-                    /> 
-                    <button>
-                        Enviar
-                    </button>
-                </form>                
-            </div>
+            { true ? "" :
+                // className={activeCard === "createContact" ? "active" : ""}
+                <div className="list-contact">
+                    <h3 className="title">Escreva sua mensagem</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Nome" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Email" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Mensagem"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>                
+                </div>
+            }
+            
+            { true ? "" :
+                // className={activeCard === "listContact" ? "active" : ""} 
+                <div className="list-contact">
+                    <h3 className="title">Escreva sua mensagem</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Nome" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Email" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Mensagem"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>                
+                </div>
+            }
+
+            { true ? "" :
+                // className={activeCard === "deleteContact" ? "active" : ""}  
+                <div className="list-contact">
+                    <h3 className="title">Escreva sua mensagem</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Nome" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Email" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Mensagem"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>                
+                </div>
+            }
             
         </ContainerContact>
     )
