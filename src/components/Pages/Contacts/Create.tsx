7885b198563caef1d852bdb632cb83acfb0b020e
Fino59 <slipdan@hotmail.com>
2021-11-19 17:42:50 -0300

    Update Contacts Pages

--- src/components/Pages/Contacts/Create.tsx
+++ src/components/Pages/Contacts/Create.tsx
@@ -5,8 +5,30 @@ import { ContainerContact } from "./style";
 const Create: React.FC = (props) => {
     return (
         <ContainerContact>
-            <h1>Nova Mensagem</h1>
+            <h1>Contatos</h1>
+
+            <div className="contacts-buttons">
+                <button 
+                    // className={activeCard === "createContact" ? "active" : ""} 
+                    // onClick={() => setActiveCard("createContact")}
+                >
+                    Criar
+                </button>
+                <button
+                    // className={activeCard === "listContact" ? "active" : ""} 
+                    // onClick={() => setActiveCard("listContact")}
+                >
+                    Listar
+                </button>                
+                <button
+                    // className={activeCard === "deleteContact" ? "active" : ""} 
+                    // onClick={() => setActiveCard("deleteContact")}
+                >
+                    Deletar
+                </button>                
+            </div>
             <hr></hr>
+
             <div className="list-contact">
                 <h3 className="title">Escreva sua mensagem</h3>
                 <form>
--- src/components/Pages/Contacts/style.ts
+++ src/components/Pages/Contacts/style.ts
@@ -4,22 +4,53 @@ export const ContainerContact = styled.div`
     background: #E3E3E3;
     width: 100vw;
     height: 100vh;
+    padding: 100px 10px 10px 80px;
+    position: fixed; 
     h1 {
         color: #CC6237;
         margin: 0;
-        padding: 20px;
+        padding: 30px 0 10px 80px;
     }
     hr {
         border: 1px solid #CC6237;
+    }
+    .contacts-buttons {
+        width: 85%;
+        display: flex;
+        justify-content: space-between;
+        gap: 20px;
+        padding: 10px 0 20px 80px;
+        button {
+            width: 180px;
+            height: 45px;
+            padding: 15px 0;
+            background: #CC6237;
+            border-radius: 30px;
+            border: none;
+            box-sizing: border-box;
+
+            color: #FFF;
+            font-family: 'Chaney'; 
+            font-weight: normal;
+            font-size: 14px;
+
+            cursor: pointer;
+            box-shadow: 1px 1px 5px #00000059;
+            transition: 250ms ease-in-out;
+            &:hover {                
+                background: #E3E3E3;
+                color: #CC6237;
+                border: 2px solid #CC6237;
+            }
+        }
     }
     .list-contact{
         background: #E3E3E3;
         width: 100%;
         display: flex;
-        flex-direction: column;
-        align-items: center;
+        flex-direction: column; 
+        padding: 0 80px;       
         form {
-        padding: 20px;
         width: 40%;
         display: flex;
         flex-direction: column;
@@ -32,14 +63,27 @@ export const ContainerContact = styled.div`
                 border: none;
             }
             button {
-                font-family: 'Bebas Neue';
-                font-size: 20px;
-                background-color: #CC6237;
-                border-radius: 5px;
+                margin-top: 10px;
+                height: 45px;
+                padding: 15px 0;
+                background: #CC6237;
+                border-radius: 30px;
                 border: none;
-                margin: 10px 0;
-                padding: 10px;
+                box-sizing: border-box;
+
+                color: #FFF;
+                font-family: 'Chaney'; 
+                font-weight: normal;
+                font-size: 14px;
+
+                cursor: pointer;
                 box-shadow: 1px 1px 5px #00000059;
+                transition: 250ms ease-in-out;
+                &:hover {                
+                    background: #E3E3E3;
+                    color: #CC6237;
+                    border: 2px solid #CC6237;
+                }
             }
         }
     }
