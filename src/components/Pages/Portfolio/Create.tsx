24885effa7b00e03006dcb1c4060713374217b4c
Fino59 <slipdan@hotmail.com>
2021-11-19 17:43:21 -0300

    Update Portfolio Pages

--- src/components/Pages/Portfolio/Create.tsx
+++ src/components/Pages/Portfolio/Create.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import { ContainerPortfolio } from "./style";
+import Gear from '../../Svg/Gear'
+
 
 const Create: React.FC = (props) => {
     return (
         <ContainerPortfolio>
             <h1>Criar Portfólio</h1>
+            <hr></hr>
 
             <div className="list-contact">
                 <p className="title">Criar Portfólio</p>
@@ -18,6 +21,7 @@ const Create: React.FC = (props) => {
                         <th>Img tipo</th>
                         <th>Descrição</th>
                         <th>Status</th>
+                        <th>Editar</th>
                     </thead>
                     <tbody>
                         <th>Aplicativo 1</th>
@@ -27,6 +31,7 @@ const Create: React.FC = (props) => {
                         <th>Tipo imagem</th>
                         <th>Descrição do sistema</th>
                         <th>Inativo</th>
+                        <th><button className="gears"><Gear /></button></th>
                     </tbody>              
                 </table>
             </div>
--- src/components/Pages/Portfolio/style.ts
+++ src/components/Pages/Portfolio/style.ts
@@ -2,13 +2,56 @@ import styled from "styled-components"
 
 export const ContainerPortfolio = styled.div`
     background: #E3E3E3;
-    width: 90vw;
-    height: 100vh;
+    width: 100vw;
+    height: 90vh;
+    padding: 100px 10px 10px 80px;
+    position: fixed;  
     h1 {
         color: #CC6237;
+        padding: 30px 0 10px 80px;
+    }
+    hr {
+        border: 1px solid #CC6237;
     }
     .list-contact {
-        background: #E3E3E3;
-        width: 100%;
+        width: calc(100% - 60px);
+        padding: 0 80px;
+        .title {
+            font-size: 18px;
+        }
+        table {
+            background: #FFF;
+            width: 85%;
+            border: 1px solid #CC6237;
+            thead {
+                background: #D6BBA9;
+                th {
+                    border-left: 1px solid #FFF;
+                }
+            }
+            th {
+                font-weight: 500;
+                border-left: 1px solid #CC6237;                
+            }
+            th:first-child {
+                border-left: 0;
+            }
+            tbody > th {
+                .gears {
+                    background: #FFF;
+                    border: none;
+                    padding: 10px;                   
+                }
+                svg {
+                    width: 20px;
+                    height: 20px;
+                    fill: #CC6237;
+                    transition: 150ms ease-in-out;
+                    &:hover {
+                        transform: scale(1.5);
+                    }                    
+                }
+            }
+        }
     }
 `
