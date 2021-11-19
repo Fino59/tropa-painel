e3809b3becae7c0c3d4954d2a0476ec98266f1a5
Fino59 <slipdan@hotmail.com>
2021-11-19 17:42:11 -0300

    Update Banner Pages

--- src/components/Pages/Banner/Create.tsx
+++ src/components/Pages/Banner/Create.tsx
@@ -1,34 +1,144 @@
 import React from 'react'
+import List from './List'
 import { ContainerBanner } from './style'
 
 const Create: React.FC<any> = (props) => {
 
-
     return (
         <ContainerBanner>
             <h1>Gerenciar Banners</h1>
-            <hr></hr>
-            <div className="create-banner">
-                <h3 className="title">Criar Banner</h3>
-                <form>
-                    <input 
-                        type="text"                        
-                        placeholder="Título" 
-                    />
-                    <input 
-                        type="text"
-                        placeholder="Descrição da imagem" 
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Imagens: .png, .jpg"
-                    /> 
-                    <button>
-                        Enviar
-                    </button>
-                </form>
-            </div>
 
+                <div className="banner-buttons">
+                    <button 
+                        // className={activeCard === "createCard" ? "active" : ""} 
+                        // onClick={() => setActiveCard("createCard")}
+                    >
+                        Criar
+                    </button>
+                    <button
+                        // className={activeCard === "listCard" ? "active" : ""} 
+                        // onClick={() => setActiveCard("listCard")}
+                    >
+                        Listar
+                    </button>
+                    <button
+                        // className={activeCard === "listCard" ? "active" : ""} 
+                        // onClick={() => setActiveCard("listCard")}
+                    >
+                        Editar
+                    </button>
+                    <button
+                        // className={activeCard === "deleteCard" ? "active" : ""} 
+                        // onClick={() => setActiveCard("deleteCard")}
+                    >
+                        Deletar
+                    </button>
+                    <button
+                        // className={activeCard === "uploadCard" ? "active" : ""} 
+                        // onClick={() => setActiveCard("uploadCard")}
+                    >
+                        Upload
+                    </button>
+                </div>
+
+            <hr></hr>
+
+                { true ? "" : 
+                <div className="create-banner">
+                    <h3 className="title">Criar Banner</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Título" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Descrição da imagem" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Imagens: .png, .jpg"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>
+                </div>
+                }
+
+                { true ? "" : 
+                    <List />
+                }
+
+                { true ? "" : 
+                <div className="create-banner">
+                    <h3 className="title">Editar Banner</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Título" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Descrição da imagem" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Imagens: .png, .jpg"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>
+                </div>
+                }
+
+                { true ? "" : 
+                <div className="create-banner">
+                    <h3 className="title">Deletar Banner</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Título" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Descrição da imagem" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Imagens: .png, .jpg"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>
+                </div>
+                }
+
+                { true ? "" : 
+                <div className="create-banner">
+                    <h3 className="title">Fazer Upload</h3>
+                    <form>
+                        <input 
+                            type="text"                        
+                            placeholder="Título" 
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Descrição da imagem" 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Imagens: .png, .jpg"
+                        /> 
+                        <button>
+                            Enviar
+                        </button>
+                    </form>
+                </div>
+                }
+                
         </ContainerBanner>
     )
 }
--- src/components/Pages/Banner/style.ts
+++ src/components/Pages/Banner/style.ts
@@ -3,23 +3,54 @@ import styled from "styled-components"
 export const ContainerBanner = styled.div`
     background: #E3E3E3;
     width: 100vw;
-    height: 100vh;    
+    height: 90vh; 
+    padding: 100px 10px 10px 80px;
+    position: fixed;    
     h1 {
         color: #CC6237; 
         margin: 0;
-        padding: 20px;
+        padding: 20px 0 20px 80px;
     }
     hr {
         border: 1px solid #CC6237;
+    }
+    .banner-buttons {
+        width: 85%;
+        display: flex;
+        justify-content: space-between;
+        gap: 20px;
+        padding: 10px 0 20px 80px;
+        button {
+            width: 180px;
+            height: 45px;
+            padding: 15px 0;
+            background: #CC6237;
+            border-radius: 30px;
+            border: none;
+            box-sizing: border-box;
+
+            color: #FFF;
+            font-family: 'Chaney'; 
+            font-weight: normal;
+            font-size: 14px;
+
+            cursor: pointer;
+            box-shadow: 1px 1px 5px #00000059;
+            transition: 250ms ease-in-out;
+            &:hover {                
+                background: #E3E3E3;
+                color: #CC6237;
+                border: 2px solid #CC6237;
+            }
+        }
     }
     .create-banner{        
         background: #E3E3E3;
         width: 100%;
         display: flex;
-        flex-direction: column;
-        align-items: center;
-        form {
-        padding: 20px;
+        flex-direction: column;        
+        padding: 0 80px;
+        form {        
         width: 40%;
         display: flex;
         flex-direction: column;
@@ -40,6 +71,10 @@ export const ContainerBanner = styled.div`
                 margin: 10px 0;
                 padding: 10px;
                 box-shadow: 1px 1px 5px #00000059;
+                transition: 150ms ease-in-out ;
+                &:hover {
+                    opacity: 0.8;
+                }
             }
         }
     }
