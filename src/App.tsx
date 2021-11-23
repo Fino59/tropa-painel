2b482e3339b260f546551f9030bc84a06dc5e969
Fino59 <slipdan@hotmail.com>
2021-11-23 17:40:57 -0300

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
 import Routing from './Routes';
 
 function App() {
